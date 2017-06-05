bad8dea88f94ad0c5467072b4fd6b3dc87556a4f
huangyu0522 <384539643@qq.com>
2017-06-05 09:30:33 +0800

    将文件打开的Mimetype封装为工具类

--- app/src/main/java/com/huangyu/mdfolder/mvp/model/FileModel.java
+++ app/src/main/java/com/huangyu/mdfolder/mvp/model/FileModel.java
@@ -8,210 +8,14 @@ import android.support.v4.content.FileProvider;
 
 import com.huangyu.library.mvp.IBaseModel;
 import com.huangyu.library.util.FileUtils;
+import com.huangyu.mdfolder.utils.MimeTypeUtils;
 
 import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Created by huangyu on 2017-5-24.
  */
 public class FileModel implements IBaseModel {
-
-    private Map<String, String> mimeTypeMap;
-
-    public FileModel() {
-        mimeTypeMap = new HashMap<>();
-        mimeTypeMap.put(
-                ".3gp", "video/3gpp"
-        );
-        mimeTypeMap.put(
-                ".apk", "application/vnd.android.package-archive"
-        );
-        mimeTypeMap.put(
-                ".asf", "video/x-ms-asf"
-        );
-        mimeTypeMap.put(
-                ".avi", "video/x-msvideo"
-        );
-        mimeTypeMap.put(
-                ".bin", "application/octet-stream"
-        );
-        mimeTypeMap.put(
-                ".bmp", "image/bmp"
-        );
-        mimeTypeMap.put(
-                ".c", "text/plain"
-        );
-        mimeTypeMap.put(
-                ".class", "application/octet-stream"
-        );
-        mimeTypeMap.put(
-                ".conf", "text/plain"
-        );
-        mimeTypeMap.put(
-                ".cpp", "text/plain"
-        );
-        mimeTypeMap.put(
-                ".doc", "application/msword"
-        );
-        mimeTypeMap.put(
-                ".exe", "application/octet-stream"
-        );
-        mimeTypeMap.put(
-                ".gif", "image/gif"
-        );
-        mimeTypeMap.put(
-                ".gtar", "application/x-gtar"
-        );
-        mimeTypeMap.put(
-                ".gz", "application/x-gzip"
-        );
-        mimeTypeMap.put(
-                ".h", "text/plain"
-        );
-        mimeTypeMap.put(
-                ".htm", "text/html"
-        );
-        mimeTypeMap.put(
-                ".html", "text/html"
-        );
-        mimeTypeMap.put(
-                ".jar", "application/java-archive"
-        );
-        mimeTypeMap.put(
-                ".java", "text/plain"
-        );
-        mimeTypeMap.put(
-                ".jpeg", "image/jpeg"
-        );
-        mimeTypeMap.put(
-                ".jpg", "image/jpeg"
-        );
-        mimeTypeMap.put(
-                ".js", "application/x-javascript"
-        );
-        mimeTypeMap.put(
-                ".log", "text/plain"
-        );
-        mimeTypeMap.put(
-                ".m3u", "audio/x-mpegurl"
-        );
-        mimeTypeMap.put(
-                ".m4a", "audio/mp4a-latm"
-        );
-        mimeTypeMap.put(
-                ".m4b", "audio/mp4a-latm"
-        );
-        mimeTypeMap.put(
-                ".m4p", "audio/mp4a-latm"
-        );
-        mimeTypeMap.put(
-                ".m4u", "video/vnd.mpegurl"
-        );
-        mimeTypeMap.put(
-                ".m4v", "video/x-m4v"
-        );
-        mimeTypeMap.put(
-                ".mov", "video/quicktime"
-        );
-        mimeTypeMap.put(
-                ".mp2", "audio/x-mpeg"
-        );
-        mimeTypeMap.put(
-                ".mp3", "audio/x-mpeg"
-        );
-        mimeTypeMap.put(
-                ".mp4", "video/mp4"
-        );
-        mimeTypeMap.put(
-                ".mpc", "application/vnd.mpohun.certificate"
-        );
-        mimeTypeMap.put(
-                ".mpe", "video/mpeg"
-        );
-        mimeTypeMap.put(
-                ".mpeg", "video/mpeg"
-        );
-        mimeTypeMap.put(
-                ".mpg", "video/mpeg"
-        );
-        mimeTypeMap.put(
-                ".mpg4", "video/mp4"
-        );
-        mimeTypeMap.put(
-                ".mpga", "audio/mpeg"
-        );
-        mimeTypeMap.put(
-                ".msg", "application/vnd.ms-outlook"
-        );
-        mimeTypeMap.put(
-                ".ogg", "audio/ogg"
-        );
-        mimeTypeMap.put(
-                ".pdf", "application/pdf"
-        );
-        mimeTypeMap.put(
-                ".png", "image/png"
-        );
-        mimeTypeMap.put(
-                ".pps", "application/vnd.ms-powerpoint"
-        );
-        mimeTypeMap.put(
-                ".ppt", "application/vnd.ms-powerpoint"
-        );
-        mimeTypeMap.put(
-                ".prop", "text/plain"
-        );
-        mimeTypeMap.put(
-                ".rar", "application/x-rar-compressed"
-        );
-        mimeTypeMap.put(
-                ".rc", "text/plain"
-        );
-        mimeTypeMap.put(
-                ".rmvb", "audio/x-pn-realaudio"
-        );
-        mimeTypeMap.put(
-                ".rtf", "application/rtf"
-        );
-        mimeTypeMap.put(
-                ".sh", "text/plain"
-        );
-        mimeTypeMap.put(
-                ".tar", "application/x-tar"
-        );
-        mimeTypeMap.put(
-                ".tgz", "application/x-compressed"
-        );
-        mimeTypeMap.put(
-                ".txt", "text/plain"
-        );
-        mimeTypeMap.put(
-                ".wav", "audio/x-wav"
-        );
-        mimeTypeMap.put(
-                ".wma", "audio/x-ms-wma"
-        );
-        mimeTypeMap.put(
-                ".wmv", "audio/x-ms-wmv"
-        );
-        mimeTypeMap.put(
-                ".wps", "application/vnd.ms-works"
-        );
-        mimeTypeMap.put(
-                ".xml", "text/plain"
-        );
-        mimeTypeMap.put(
-                ".z", "application/x-compress"
-        );
-        mimeTypeMap.put(
-                ".zip", "application/zip"
-        );
-        mimeTypeMap.put(
-                "", "*/*"
-        );
-    }
 
     /**
      * 打开文件
@@ -236,7 +40,7 @@ public class FileModel implements IBaseModel {
     }
 
     private String getMIMEType(File file) {
-        return mimeTypeMap.get(FileUtils.getFileExtension(file));
+        return MimeTypeUtils.getMIMEType(file.getPath());
     }
 
     public boolean isFileExists(String path) {
