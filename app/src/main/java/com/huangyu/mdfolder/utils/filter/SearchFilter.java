e1bbc5d32c8ad838ea56cccf9ab12121e00957a9
huangyu0522 <384539643@qq.com>
2017-06-20 18:04:13 +0800

    查询忽略大小写匹配

--- app/src/main/java/com/huangyu/mdfolder/utils/filter/SearchFilter.java
+++ app/src/main/java/com/huangyu/mdfolder/utils/filter/SearchFilter.java
@@ -21,7 +21,28 @@ public class SearchFilter implements FilenameFilter {
         if (TextUtils.isEmpty(mSearchStr)) {
             return !name.startsWith(".");
         }
-        return name.contains(mSearchStr) && !name.startsWith(".");
+        return containsIgnoreCase(name, mSearchStr) && !name.startsWith(".");
+    }
+
+    /**
+     * 忽略大小写匹配
+     *
+     * @param str
+     * @param searchStr
+     * @return
+     */
+    private static boolean containsIgnoreCase(String str, String searchStr) {
+        if (str == null || searchStr == null) return false;
+
+        final int length = searchStr.length();
+        if (length == 0)
+            return true;
+
+        for (int i = str.length() - length; i >= 0; i--) {
+            if (str.regionMatches(true, i, searchStr, 0, length))
+                return true;
+        }
+        return false;
     }
 
 }
