b0a3355aaa96d1fd8168f89d57cab8c4156a9a1f
huangyu0522 <384539643@qq.com>
2017-06-26 00:15:23 +0800

    修复按时间排序出错的问题

--- app/src/main/java/com/huangyu/mdfolder/utils/comparator/TimeComparator.java
+++ app/src/main/java/com/huangyu/mdfolder/utils/comparator/TimeComparator.java
@@ -1,10 +1,8 @@
 package com.huangyu.mdfolder.utils.comparator;
 
 import com.huangyu.mdfolder.bean.FileItem;
-import com.huangyu.mdfolder.utils.DateUtils;
 
 import java.util.Comparator;
-import java.util.Date;
 
 /**
  * Created by huangyu on 2017-5-24.
@@ -12,9 +10,7 @@ import java.util.Date;
 public class TimeComparator implements Comparator<FileItem> {
 
     public int compare(FileItem file1, FileItem file2) {
-        Date date1 = DateUtils.stringToDate(file1.getDate());
-        Date date2 = DateUtils.stringToDate(file2.getDate());
-        long diff = date1.getTime() - date2.getTime();
+        long diff = Long.valueOf(file1.getDate()) - Long.valueOf(file2.getDate());
         if (diff < 0) {
             return -1;
         } else if (diff > 0) {
